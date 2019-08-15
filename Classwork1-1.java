d43fd8fa1741d79d301f707a841c5f9f4b56f001
Mia Tabladillo <52267579+mianoelani@users.noreply.github.com>
2019-08-15 13:27:57 -0700

    Create Classwork1-1.java

---
+++ Classwork1-1.java
@@ -0,0 +1,21 @@
+/**
+ * first program-print statement
+ *
+ * @Mia Tabladillo
+ * @8/13/19
+ */
+public class InClass1
+{
+    
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * prints hello world
+     */
+    public InClass1()
+    {
+        System.out.print("Hello World!");
+        x = 0;
+    }
+}
